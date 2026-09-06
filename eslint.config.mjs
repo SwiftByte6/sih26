bef7ddc7a47c8bcbb3cca51d3de3021a27e4fb7c
Sukumarsawant <sawantsukumar@gmail.com>
2026-09-06 14:36:49 +0530

    nit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ const eslintConfig = defineConfig([
     rules: {
       "react/no-unescaped-entities": "off",
       "react-hooks/set-state-in-effect": "warn",
+      "react-hooks/rules-of-hooks": "warn",
       "react-hooks/rules-of-hooks": "error",
     },
   },
